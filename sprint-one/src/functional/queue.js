5d60e3f768e574fb811785c640cb788bd9a8e5ea
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-08 15:38:18 -0800

    pass all tests in The Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var nextInLine = 1;
+  var endOfLine = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //add value to storage
+
+    storage[endOfLine + 1] = value;
+    endOfLine++;
+
   };
 
   someInstance.dequeue = function() {
+    var dequeuedValue = storage[nextInLine];
+
+    delete storage[nextInLine];
+    nextInLine++;
+    return dequeuedValue;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var keys in storage) {
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
