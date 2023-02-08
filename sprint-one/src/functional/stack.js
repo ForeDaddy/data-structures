5a383221b3eda8e895391a134e3185d7a7807c58
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-08 12:32:43 -0800

    create size function in Functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,31 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //I: value
+    //O: none, side effect var storage changed
+    //C: no arrays
+    //E: ?
+
+    //add value to top of stack
+    //this will allow the last value added to the stack to be found at the top
+    //using the key '1' for bottom of stack with the size function will allow for correct assignment of pushed val keys
+    var topStack = someInstance.size(storage);
+    storage[topStack + 1] = value;
+
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    //create count and iterate through keys
+    var count = 0;
+    for (var keys in storage) {
+      count++;
+    }
+    return count;
+      //increment count
+    //return count
   };
 
   return someInstance;
