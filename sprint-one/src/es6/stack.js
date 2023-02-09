d72c2b14285111f4d45a285e6b8e085e829510f3
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-09 11:38:11 -0800

    passed all tests in es6 and all tests in specRunner.html

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var topOfStack = this.size(this.storage);
+    this.storage[topOfStack + 1] = value;
+
+  }
+
+  pop() {
+    var topOfStack = this.size(this.storage);
+    var removedFromStack = this.storage[topOfStack];
+    delete this.storage[topOfStack];
+    return removedFromStack;
+
+  }
+
+  size() {
+    var count = 0;
+    for (var keys in this.storage) {
+      count++
+    }
+    return count;
   }
 
 }
