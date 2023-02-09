dff5e3770e49d41eab280c1acdafea51937502bb
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-09 11:26:04 -0800

    pass all tests in pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  var topOfStack = this.size(this.storage);
+  this.storage[topOfStack + 1] = value;
+}
+
+Stack.prototype.pop = function() {
+  var topOfStack = this.size(this.storage);
+  var removedFromStack = this.storage[topOfStack];
+  delete this.storage[topOfStack];
+  return removedFromStack;
+
+};
+
+Stack.prototype.size = function() {
+  var count = 0;
+  for (var key in this.storage) {
+    count++;
+  }
+  return count;
+}
 
 
