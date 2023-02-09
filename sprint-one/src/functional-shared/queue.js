171e566a97bf0fd8939b3bbf33a93fb97806c374
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-08 16:50:20 -0800

    queue.js has skeleton in The Functional Shared Pattern and is passing 22 of 38 tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.counts = {firstInLine: 0, lastInLine: 0};
+  someInstance.storage = {};
+  _.extend(someInstance, Queue.queueMethods);
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
+Queue.queueMethods = {};
+
+Queue.queueMethods.enqueue = function(value) {
+  var nextQueuePosition = this.counts.lastInLine;
+  this.storage[nextQueuePosition + 1] = value;
+  this.counts.lastInLine++;
+}
+
+Queue.queueMethods.dequeue = function() {
+  var firstQueuePosition = this.counts.firstInLine;
+  var removedFromQueue = this.storage[firstQueuePosition];
+  delete this.storage[firstQueuePosition];
+  this.counts.firstInLine++;
+  return removedFromQueue;
+}
+
+Queue.queueMethods.size = function() {
+  this.counts.countSize = 0;
+  for (var keys in this.storage) {
+    this.counts.countSize++;
+  }
+  return this.counts.countSize;
+}
 
 
