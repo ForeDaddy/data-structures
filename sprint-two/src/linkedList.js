7d3dd0591893300d8b7f6cdf87049a7c5f09dd4d
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-09 18:53:03 -0800

    pass all linkedlist.js tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,77 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new node
+    //list.tail.next ---> new node
+    // list.tail = new node
+    //create new node
+    var newNode = Node(value);
+    if (list.tail === null) {
+    list.tail = Node(value);
+    }
+    if (list.head === null) {
+      list.head = newNode;
+    }
+
+
+      list.tail.next = newNode;
+      list.tail = newNode;
+
+    // var newNode = Node(value)
+    // list.tail.next = newNode
+    // list.tail = newNode
+    //change list.tail to new node
+
+
   };
 
   list.removeHead = function() {
+    //check node at list.head
+      //if node.next != null
+      //list.head = node.next;
+    if (!list.head) {
+      return null;
+    }
+    var head = list.head.value;
+
+    list.head = list.head.next;
+
+
+    return head;
   };
 
   list.contains = function(target) {
+    // target equals value
+    // base case
+    // if .value equals target
+      // return true;
+
+    // recursion case
+    // .value equals false
+      //
+
+      //inner function
+      //innerfunction(.head)
+      var innerFunction = function (target, node) {
+        if (node.value === target) {
+          return true;
+        } else if (!node.next) {
+          return false;
+        } else {
+          return innerFunction(target, node.next);
+        }
+      }
+    return innerFunction(target, list.head);
+        // iterate through each node
+    // start at list.head
+    // if node.value equals true
+      // result equals true
+      // return
+    // go to node.next
+
+      // check if target equals current value, if so
+        // return true
+  // return false;
   };
 
   return list;
