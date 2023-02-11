ed378b9fee75f744aecfc35445236a8719ab44b9
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-11 15:40:14 -0800

    pass all provided tests in part 2

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,6 +2,7 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._tupleCount = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -39,6 +40,9 @@ HashTable.prototype.insert = function(k, v) {
     }
 
   }
+  this._tupleCount++;
+  this.doubleHash();
+  // console.log('tuple count: ', this._tupleCount);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -48,8 +52,8 @@ HashTable.prototype.retrieve = function(k) {
     var bucket = this._storage.get(index);
 
     // check if bucket:this._storage.get(index) is longer than 1
-    if (this._storage.get(index).length > 1) {
-      var length = this._storage.get(index).length;
+    if (bucket.length > 1) {
+      var length = bucket.length;
       // iterate through bucket
       for (var i = 0; i < length; i++) {
         var tupleKey = bucket[i][0];
@@ -61,7 +65,6 @@ HashTable.prototype.retrieve = function(k) {
         }
       }
     }
-
     return bucket[0][1];
   }
 };
@@ -69,7 +72,89 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(index, undefined);
+  this._tupleCount--;
+  this.halveHash();
+};
 
+
+HashTable.prototype.doubleHash = function() {
+  // is tupleCount more than 75% limit
+  if (this._tupleCount > (Math.floor(.75 * this._limit))) {
+    // console.log('more than ', Math.floor(.75 * this._limit));
+    this.increaseSize();
+  }
+    // run increaseSize
+
+};
+HashTable.prototype.halveHash = function() {
+  // is tupleCount less than 25% limit
+  if (this._tupleCount < (Math.floor(.25 * this._limit))) {
+    // console.log('less than ', Math.floor(.25 * this._limit));
+    this.decreaseSize();
+  }
+}
+    // run decreaseSize
+
+HashTable.prototype.increaseSize = function() {
+  // double limit
+  var newStorage = LimitedArray(this._limit*2);
+  var tempTupleArray = [];
+  // iterate through this._storage
+  for (let i = 0; i < this._limit; i++) {
+    // if bucket
+    let bucket = this._storage.get(i);
+    if (bucket) {
+      // iterate through each bucket index
+      for (let i = 0; i < bucket.length; i++) {
+        // for each item, push to storage array
+        tempTupleArray.push(bucket[i]);
+      }
+    }
+  }
+
+  this._storage = newStorage;
+  this._limit = this._limit*2;
+  this._tupleCount = 0;
+
+  // iterate through tuple array
+  for (let i = 0; i < tempTupleArray.length; i++) {
+    // run the .insert function against tuple k, v
+    let tupleKey = tempTupleArray[i][0];
+    let tupleValue = tempTupleArray[i][1];
+    this.insert(tupleKey, tupleValue);
+  }
+
+};
+
+HashTable.prototype.decreaseSize = function() {
+// iterate through this._storage
+  var newStorage = LimitedArray(Math.floor(this._limit/2));
+  var tempTupleArray = [];
+  // iterate through this._storage
+  for (let i = 0; i < this._limit; i++) {
+    // if bucket
+    let bucket = this._storage.get(i);
+    if (bucket) {
+      // iterate through each bucket index
+      for (let i = 0; i < bucket.length; i++) {
+        // for each item, push to storage array
+        tempTupleArray.push(bucket[i]);
+      }
+    }
+  }
+
+  this._storage = newStorage;
+  this._limit = Math.floor(this._limit/2);
+  console.log('limit: ', this._limit);
+  this._tupleCount = 0;
+
+  // iterate through tuple array
+  for (let i = 0; i < tempTupleArray.length; i++) {
+    // run the .insert function against tuple k, v
+    let tupleKey = tempTupleArray[i][0];
+    let tupleValue = tempTupleArray[i][1];
+    this.insert(tupleKey, tupleValue);
+  }
 };
 
 /*
